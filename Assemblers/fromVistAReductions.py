0c3f3ee20de8093152c512c1a414a02dc2510ec7
conorcaregraf <conor-dowling@caregraf.com>
2019-02-08 18:56:33 -0800

    ...

--- Assemblers/fromVistAReductions.py
+++ Assemblers/fromVistAReductions.py
@@ -12,10 +12,52 @@ from fmqlutils.reporter.reportUtils import MarkdownTable, reportPercent, reportA
 
 VISTA_RPCD_LOCN_TEMPL = "/data/vista/{}/RPCDefinitions/"
 
-SNOS = ["442", "640", "999"]
+SNOS = ["442", "640", "999"] # SHOULD PUT DATE ON THESE
 
-def assemble(): # first just compare contents
-    pass
+"""
+First just keep first copy of RPC and tag others have it. 
+
+TODO: validate consistency
+"""
+def assemble(): 
+    
+    definitionsByRPC = OrderedDict()
+    
+    for sno in SNOS:
+        
+        _8994Reductions = json.load(open(VISTA_RPCD_LOCN_TEMPL.format(sno) + "_8994Reduction.json"))
+        
+        for red in _8994Reductions:
+            
+            if red["label"] in definitionsByRPC:
+                definition = definitionsByRPC[red["label"]]
+                definition["_vistas"].append(sno)
+                continue
+                
+            red["_vistas"] = [sno]
+            rpc = red["label"]
+            del red["label"]
+            definitionsByRPC[rpc] = red
+        
+    """
+    \# | Metric | Count
+    --- | --- | ---
+    1 | Total RPCs | 5,475
+    2 | In all 3 | 3,669
+    3 | In all 442/640 but not FOIA | 1,481
+    4 | Exclusive 442 | 87
+    5 | Exclusive 640 | 231
+    6 | Exclusive 999 | 5
+    """
+    tbl = MarkdownTable(["Metric", "Count"])
+    tbl.addRow(["Total RPCs", len(definitionsByRPC)])
+    tbl.addRow(["In all 3", sum(1 for rpc in definitionsByRPC if len(definitionsByRPC[rpc]["_vistas"]) == 3)])
+    tbl.addRow(["In all 442/640 but not FOIA", sum(1 for rpc in definitionsByRPC if set(definitionsByRPC[rpc]["_vistas"]) == set(["442", "640"]))])
+    for sno in SNOS:
+        tbl.addRow(["Exclusive {}".format(sno), sum(1 for rpc in definitionsByRPC if len(definitionsByRPC[rpc]["_vistas"]) == 1 and sno in definitionsByRPC[rpc]["_vistas"])])
+    print tbl.md()
+        
+    json.dump(definitionsByRPC, open("../Definitions/rpcInterfaceDefinition.json", "w"), indent=4)
 
 # ################################# DRIVER #######################
                
@@ -23,13 +65,7 @@ def main():
 
     assert(sys.version_info >= (2,7))
 
-    if len(sys.argv) < 2:
-        print "need to specify station # ex/ 442 - exiting"
-        return
-        
-    stationNo = sys.argv[1]
-    
-    reduceFMData(stationNo)
+    assemble()
 
 if __name__ == "__main__":
     main()
